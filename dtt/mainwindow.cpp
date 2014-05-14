2810af80a28dd234096d2215c88b3271709df4d9
Grégoire Barbier <devel@g76r.eu>
2014-05-14 17:29:55 +0200

    dtt: must close any subwindow before main window

--- dtt/mainwindow.cpp
+++ dtt/mainwindow.cpp
@@ -15,8 +15,14 @@ MainWindow::MainWindow(DocumentManager *documentManager, QWidget *parent)
 MainWindow::~MainWindow() {
 }
 
+// LATER check if document should be saved and ignore event do prevent closing
 void MainWindow::closeEvent(QCloseEvent *event) {
-  // LATER check if document should be saved here and ignore event do prevent closing
+  // closing subwindows before main window is needed because subwindows may
+  // rely on resources allocated by main window, such as the DocumentManager,
+  // some QAbstractItemModels, loaded configuration, etc.
+  foreach (QWidget *w, documentManager()->perspectiveWidgets())
+    if (!w->parent())
+      w->close();
   // Note: creating a label here is a hack to ensure that MainWindow and chidren
   // destructors will all be called, even on platforms (at less Windows) where
   // closing the last window kills the program
