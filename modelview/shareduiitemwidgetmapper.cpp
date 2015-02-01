b4b05809765508ad4fc2440aa1fb97fa0100370c
Grégoire Barbier <devel@g76r.eu>
2015-02-01 23:08:54 +0100

    modelview: SharedUiItemWidgetMapper handling EditRole like Qt's views
    i.e. if EditRole data is null, no longer request DisplayRole as a
    replacement

--- modelview/shareduiitemwidgetmapper.cpp
+++ modelview/shareduiitemwidgetmapper.cpp
@@ -82,8 +82,6 @@ void SharedUiItemWidgetMapper::populate(int section) {
     if (!propname.isEmpty()) {
       QVariant v = _item.uiData(section, Qt::EditRole);
       if (v.isNull())
-        v = _item.uiData(section, Qt::DisplayRole);
-      if (v.isNull())
         v = _sectionToDefaultValue.value(section);
       widget->setProperty(propname, v);
     }
