1c8378b1b30fcc95f42b6f013a2cb458b65bf140
Grégoire Barbier <devel@g76r.eu>
2015-09-02 23:55:11 +0200

    modelview/dtp: do not push no-op undo commands

--- dtp/dtpdocumentmanager.cpp
+++ dtp/dtpdocumentmanager.cpp
@@ -69,17 +69,14 @@ bool DtpDocumentManager::changeItem(
     QString *errorString) {
   Q_ASSERT(newItem.isNull() || newItem.idQualifier() == idQualifier);
   Q_ASSERT(oldItem.isNull() || oldItem.idQualifier() == idQualifier);
-  // support for transforming into update an createOrUpdate call
-  // and ensure that, on update or delete, oldItem is the real one, not a
-  // placeholder only bearing ids
-  oldItem = itemById(idQualifier, oldItem.id());
-  if (oldItem.isNull() && newItem.isNull())
-    return true; // nothing to do (support deleteIfExists on inexistent item)
   CoreUndoCommand *command =
       internalChangeItem(newItem, oldItem, idQualifier, errorString);
   //qDebug() << "DtpDocumentManager::changeItem" << command;
   if (command) {
-    undoStack()->push(new UndoCommandAdapter(command));
+    // commands w/o children do nothing (e.g. deleteIfExists on inexistent item)
+    // and therefore must not be pushed on undo stack but are not errors
+    if (command->childCount() > 0)
+      undoStack()->push(new UndoCommandAdapter(command));
     return true;
   }
   return false;
