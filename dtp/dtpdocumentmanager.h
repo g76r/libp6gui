575037548c45659f4f2cded56b5f1861fcb7568a
Grégoire Barbier <devel@g76r.eu>
2015-09-22 15:56:03 +0200

    adding copyright notices and license information

--- dtp/dtpdocumentmanager.h
+++ dtp/dtpdocumentmanager.h
@@ -1,3 +1,16 @@
+/* Copyright 2014-2015 Hallowyn and others.
+ * This file is part of libh6ncsu, see <https://gitlab.com/g76r/libh6ncsu>.
+ * Libh6ncsu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Libh6ncsu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with libh6ncsu.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef DTPDOCUMENTMANAGER_H
 #define DTPDOCUMENTMANAGER_H
 
