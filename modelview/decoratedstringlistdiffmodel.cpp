73d3be3374be8114116e8eccf92df2cf0e486826
Grégoire Barbier <devel@g76r.eu>
2022-09-11 19:55:45 +0200

    fixing headers, macros and inclues for libpumpkin

--- modelview/decoratedstringlistdiffmodel.cpp
+++ modelview/decoratedstringlistdiffmodel.cpp
@@ -1,15 +1,15 @@
-/* Copyright 2017 Hallowyn and others.
- * This file is part of libh6ncsu, see <https://gitlab.com/g76r/libh6ncsu>.
- * Libh6ncsu is free software: you can redistribute it and/or modify
+/* Copyright 2017-2022 Hallowyn and others.
+ * This file is part of libpumpkin, see <http://libpumpkin.g76r.eu/>.
+ * libpumpkin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * Libh6ncsu is distributed in the hope that it will be useful,
+ * libpumpkin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  * You should have received a copy of the GNU Affero General Public License
- * along with libh6ncsu.  If not, see <http://www.gnu.org/licenses/>.
+ * along with libpumpkin.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "decoratedstringlistdiffmodel.h"
 #include <QBrush>
